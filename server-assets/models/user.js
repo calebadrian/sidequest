05a8b4c76fce48ad601fc805f3f8e8e62fc1e415
Joe Davis <joecrush@msn.com>
2018-02-26 15:27:18 -0700

    implement user auth

--- server-assets/models/user.js
+++ server-assets/models/user.js
@@ -1,17 +1,43 @@
 var mongoose = require("mongoose");
+var bcrypt = require("bcryptjs");
 var Schema = mongoose.Schema;
+var ObjectId = mongoose.SchemaTypes.ObjectId;
 var schemaName = "User";
+const SALT_FACTOR = 13;
+
+var roles = ["public", "moderator", "admin"];
 
 var schema = new Schema({
- name: { type: String, required: true },
- avatar: {type: String, default: '//robohash.org'+this.name+".png"},
- username: {type: String, required: true},
- //password: {type: Number}   optinoal to add later
-
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true, minlength: 6 },
+  created: { type: Number, default: Date.now(), required: true },
+  bio: { type: String },
+  role: { type: String, enum: roles }
 });
 
+schema.statics.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(SALT_FACTOR));
+};
 
+schema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
-
+schema.methods.changeRole = function(newRole, uid) {
+  if (roles.indexOf(newRole) <= roles.indexOf(this.user.role)) {
+    this.user.role = newRole;
+    this.save();
+  }
+};
 
 module.exports = mongoose.model(schemaName, schema);
+
+
+// var schema = new Schema({
+//  name: { type: String, required: true },
+//  avatar: {type: String, default: '//robohash.org'+this.name+".png"},
+//  username: {type: String, required: true},
+//  //password: {type: Number}   optinoal to add later
+
+// });
